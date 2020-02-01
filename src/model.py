7bdeac9a9bd727dba6af2316fe0be4996ebdd7aa
YanwenLin <yanwenl@andrew.cmu.edu>
2020-01-31 21:15:45 -0500

    fix add activations bug;

--- src/model.py
+++ src/model.py
@@ -1,13 +1,16 @@
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, size_list, activation):
+    def __init__(self, size_list, activation="relu"):
         super(MLP, self).__init__()
         layers = []
         self.size_list = size_list
         for i in range(len(size_list) - 2):
             layers.append(nn.Linear(size_list[i],size_list[i+1]))
-            layers.append(activation())
+            if activation is "relu":
+                layers.append(nn.ReLU())
+            else:
+                raise NotImplementedError
         layers.append(nn.Linear(size_list[-2], size_list[-1]))
         self.net = nn.Sequential(*layers)
 
