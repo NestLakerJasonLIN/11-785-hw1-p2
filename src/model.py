390454d0dadb18977797708ae53467c611bbd6ce
Yanwen Lin <yanwenl@andrew.cmu.edu>
2020-02-03 07:53:07 +0000

    add dropout;

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, size_list, activation="relu", batch_norm=True):
+    def __init__(self, size_list, activation="relu", batch_norm=True, dropout=0.1):
         super(MLP, self).__init__()
         layers = []
         self.size_list = size_list
@@ -9,6 +9,8 @@ class MLP(nn.Module):
             layers.append(nn.Linear(size_list[i],size_list[i+1]))
             if batch_norm:
                 layers.append(nn.BatchNorm1d(num_features=size_list[i+1]))
+            if dropout and (i == 3 or i == 7):
+                layers.append(nn.Dropout(dropout))
             if activation is "relu":
                 layers.append(nn.ReLU())
             else:
